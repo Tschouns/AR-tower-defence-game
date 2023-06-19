905e61fcba5aae417021fd97a6c0ef7a87e4f557
Jonas <jonas.aklin@gmail.com>
2023-06-19 22:52:14 +0200

    Added bullet explosion effects

---
+++ TowerDefenceAR/Assets/Scripts/Effects/BulletImpactParticleEffect.cs
@@ -0,0 +1,14 @@
+﻿using Assets.Scripts.Guns;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Effects
+{
+    public class BulletImpactParticleEffect : BaseHitEffect
+    {
+        public override void Trigger(IBullet bullet, Vector3 hitPoint)
+        {
+            Instantiate(bullet.ExplosionPrefab, hitPoint, Quaternion.identity);
+        }
+    }
+}
