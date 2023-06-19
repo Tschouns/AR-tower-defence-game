ccd77b62ec38290250bc9c4c0ceb59cd54633554
Jonas <jonas.aklin@gmail.com>
2023-06-19 15:34:17 +0200

    Added game controller

---
+++ TowerDefenceAR/Assets/Scripts/Game/IGame.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Assets.Scripts.Game
+{
+    /// <summary>
+    /// Represents the game. Provides game status info.
+    /// </summary>
+    public interface IGame
+    {
+        bool IsStarted { get; }
+        bool IsLost { get; }
+        bool IsTowerBuilt { get; }
+        bool IsEnemySpawnPointBuilt { get; }
+        int DefencesBuilt { get; }
+        int ObstaclesBuilt { get; }
+        bool HasDefencesLeftToBuild { get; }
+        bool HasObstaclesLeftToBuild { get; }
+    }
+}
